6b5fdac95c422f937b1c17a1cd91304036674f43
Matthew Eldredge <mattheweldredge@gmail.com>
2019-02-10 15:40:10 -0800

    update

--- src/js/menuToggle.js
+++ src/js/menuToggle.js
@@ -3,13 +3,19 @@ export class MenuToggle {
 	toggleMenu() {
 
 		const nav = document.querySelector('.nav');
-		const menu = document.querySelector('.button');
+		const button = document.querySelector('.button');
+		const navList = document.querySelector('.nav__list');
+		const navListItem = document.querySelectorAll('.nav__list-item');
+		const navListItemLink = document.querySelectorAll('.nav__list-item-link');
 
 		const toggle = () => {
-			nav.classList.toggle('flex-nav');
-			menu.classList.toggle('change');
+			nav.classList.toggle('modalnav');
+			button.classList.toggle('change');
+			navList.classList.toggle('nav__list--modal');
+			navListItem.forEach(item => item.classList.toggle('nav__list-item--modal'));
+			navListItemLink.forEach(item => item.classList.toggle('nav__list-item-link--modal'));
 		}
 
-		nav.addEventListener('click', toggle);
+		button.addEventListener('click', toggle);
 	}
 }
